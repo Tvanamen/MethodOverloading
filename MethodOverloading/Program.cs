a85dc3a0381b53dea4fb5774016b64b8f3d0d93c
Tanner Van Amen <Tvanamen@yahoo.com>
2022-07-19 18:18:11 -0600

    inital commit

---
+++ MethodOverloading/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace MethodOverloading
+{
+    internal class Program
+    {
+        public static int Add(int a, int b)
+        {
+            return a + b;
+        }
+
+        public static decimal Add(decimal a, decimal b)
+        {
+            return a + b;
+        }
+
+        public static string Add(int a, int b, bool isTrue)
+        {
+            var sum = a + b;
+
+            if (isTrue == true && sum > 1)
+            {
+                return $"{sum} Dollars";
+            }
+            else if (isTrue == true && sum == 1)
+            {
+                return $"{sum} Dollar";
+            }
+            else if (isTrue == true && sum < 1)
+            {
+                return $"{sum} Dollars";
+            }
+            else
+            {
+                return sum.ToString();
+            }
+
+        }
+        public static void Main(string[] args)
+        {
+           
+        }
+    }
+
+}
